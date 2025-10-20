5a5a3f962425aa56867c1209014c4091438c5b1f
drra1nee <semen.sabetov@yandex.ru>
2025-10-21 01:43:47 +0300

    update resolve_path

--- src/commands/resolve_path.py
+++ src/commands/resolve_path.py
@@ -3,11 +3,17 @@
 """
 
 from pathlib import Path
+import os
 
 def resolve_path(path_str) :
+    # Обработка символа "~" в path
     if not path_str or path_str == "~":
         return Path.home()
     elif path_str.startswith("~/"):
         return Path.home() / path_str[2:]
+    # Обработка path состоящих только из названия диска в Windows
+    elif os.name == 'nt' and len(path_str) == 2 and path_str[1] == ':':
+        return Path(path_str + '\\').resolve()
+    # Обработка всех остальных path
     else:
         return Path(path_str).resolve()
