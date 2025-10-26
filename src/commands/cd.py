0fc1177c135e64a2cc35a7a7343b4df7422768da
drra1nee <semen.sabetov@yandex.ru>
2025-10-26 19:40:31 +0300

    mini fix 3

--- src/commands/cd.py
+++ src/commands/cd.py
@@ -4,11 +4,11 @@ import os
 from .resolve_path import resolve_path
 
 def cd(path):
-    target = resolve_path(path)
+    p = resolve_path(path)
 
-    if not target.exists():
+    if not p.exists():
         raise FileNotFoundError(f"cd: {path}: No such file or directory")
-    if not target.is_dir():
+    if not p.is_dir():
         raise NotADirectoryError(f"cd: {path}: Not a directory")
 
-    os.chdir(target)
+    os.chdir(p)
